b584a13dd577040cd7e274b4617b6225ec2a20f3
shadyganem <shadyganem@gmail.com>
2021-11-09 01:54:17 +0200

    added a notification bar.

--- masterwork/mwMainFrame.cpp
+++ masterwork/mwMainFrame.cpp
@@ -7,12 +7,30 @@ END_EVENT_TABLE()
 
 mwMainFrame::mwMainFrame(const wxString& title, const wxPoint& pos, const wxSize& size) : wxFrame(nullptr, wxID_ANY, title, pos, size)
 {
+	// initialzing sizers
+	m_hor_sizer = new wxBoxSizer(wxHORIZONTAL);
+	m_ver_sizer = new wxBoxSizer(wxVERTICAL);
+
+	// initialzing info controls
+	m_info_bar = new wxInfoBar(this);
+	m_info_bar->SetBackgroundColour(wxColor(100, 200, 400));
+	m_ver_sizer->Add(m_info_bar, wxSizerFlags().Expand());
+	ShowInfoBarMessage("Welcome to MasterWork");
+	
+	/*
 	mwStatusBar* m_status_bar = new mwStatusBar(this, wxID_ANY, wxDefaultPosition, wxSize(200, 100));
 	m_status_bar->SetBackgroundColour(wxColor(100, 100, 200));
 	wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
 	sizer->Add(m_status_bar, 1, wxEXPAND);
+	*/
+	this->SetSizer(m_ver_sizer);
 }
 
 mwMainFrame::~mwMainFrame()
 {
+}
+
+void mwMainFrame::ShowInfoBarMessage(const wxString& msg)
+{
+	m_info_bar->ShowMessage(msg);
 }
--- masterwork/mwMainFrame.h
+++ masterwork/mwMainFrame.h
@@ -2,12 +2,23 @@
 
 #include "wx/wx.h"
 #include "mwStatusBar.h"
+#include <wx/infobar.h>
+
+
 
 class mwMainFrame : public wxFrame
 {
 public:
 	mwMainFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
 	~mwMainFrame();
+private:
+	// private method
+	void ShowInfoBarMessage(const wxString& msg);
+private:
+	// info contols
+	wxInfoBar* m_info_bar;
+	wxSizer* m_hor_sizer;
+	wxSizer* m_ver_sizer;
 private:
 	// any class wishing to process wxWidgets events must use this macro
 	DECLARE_EVENT_TABLE()
