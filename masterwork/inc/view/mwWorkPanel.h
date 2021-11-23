a7027e38f3bfb63b665281c81facc22a0a122477
shadyganem <shadyganem@gmail.com>
2021-11-23 14:38:45 +0200

    added a notebook container to the workpanel

--- masterwork/inc/view/mwWorkPanel.h
+++ masterwork/inc/view/mwWorkPanel.h
@@ -3,11 +3,23 @@
 #include <wx/toolbar.h>
 #include "controller/mwController.h"
 #include "mwDefines.h"
+#include <wx/panel.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+#include <wx/string.h>
+#include <wx/bitmap.h>
+#include <wx/image.h>
+#include <wx/icon.h>
+#include <wx/notebook.h>
+#include <wx/sizer.h>
 
 
 class mwWorkPanel : public wxPanel
 {
 public:
+    
 	mwWorkPanel(wxWindow* parent,
                 wxWindowID winid = wxID_ANY,
                 const wxPoint& pos = wxDefaultPosition,
@@ -19,6 +31,16 @@ public:
 public:
     wxToolBar* m_tool_bar;
     wxColor m_tool_bar_bg = wxColor(0, 0, 255);
+    wxNotebook* m_notebook;
+    wxPanel* m_tasks;
+protected:
+	wxBoxSizer* m_ver_sizer;
+
+	// Virtual event handlers, overide them in your derived class
+	virtual void OnPageChanged(wxNotebookEvent& event);
+	virtual void OnPageChanging(wxNotebookEvent& event);
+
+
 
 private:
     DECLARE_EVENT_TABLE()
--- masterwork/src/mwWorkPanel.cpp
+++ masterwork/src/mwWorkPanel.cpp
@@ -11,8 +11,39 @@ mwWorkPanel::mwWorkPanel(wxWindow* parent,
 	                     long style, 
 	                     const wxString& name) : wxPanel(parent, winid, pos, size, style, name)
 {
+	this->SetBackgroundColour(wxColour(37, 37, 38));
+
+	m_ver_sizer = new wxBoxSizer(wxVERTICAL);
+
+	m_notebook = new wxNotebook(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNB_TOP);
+	m_tasks = new wxPanel(m_notebook, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNO_BORDER | wxTAB_TRAVERSAL | wxVSCROLL);
+	m_tasks->SetBackgroundColour(wxColour(37, 37, 38));
+
+	m_notebook->AddPage(m_tasks, wxT("Tasks"), false);
+
+	m_ver_sizer->Add(m_notebook, 1, wxEXPAND | wxALL, 5);
+
+	this->SetSizer(m_ver_sizer);
+	this->Layout();
+
+	// Connect Events
+	m_notebook->Connect(wxEVT_COMMAND_NOTEBOOK_PAGE_CHANGED, wxNotebookEventHandler(mwWorkPanel::OnPageChanged), NULL, this);
+	m_notebook->Connect(wxEVT_COMMAND_NOTEBOOK_PAGE_CHANGING, wxNotebookEventHandler(mwWorkPanel::OnPageChanging), NULL, this);
+}
+
+void mwWorkPanel::OnPageChanged(wxNotebookEvent& event)
+{
+	event.Skip();
+}
+
+void mwWorkPanel::OnPageChanging(wxNotebookEvent& event)
+{
+	event.Skip();
 }
 
 mwWorkPanel::~mwWorkPanel()
 {
+	// Disconnect Events
+	m_notebook->Disconnect(wxEVT_COMMAND_NOTEBOOK_PAGE_CHANGED, wxNotebookEventHandler(mwWorkPanel::OnPageChanged), NULL, this);
+	m_notebook->Disconnect(wxEVT_COMMAND_NOTEBOOK_PAGE_CHANGING, wxNotebookEventHandler(mwWorkPanel::OnPageChanging), NULL, this);
 }
