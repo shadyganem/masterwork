e29b633bdcbbfd0e789c04f66990e72db7759c44
Shady Ganem <shady.ganem@intel.com>
2023-07-28 22:37:20 +0300

    added text controls to the new reminder form

--- masterwork/inc/view/NewReminderFrame.h
+++ masterwork/inc/view/NewReminderFrame.h
@@ -1,6 +1,10 @@
 #pragma once
+
 #include <wx/frame.h>
+#include <wx/button.h>
+#include <wx/textctrl.h>
 #include "model/Reminder.h"
+#include "wx/wx.h"
 
 namespace mw
 {
@@ -8,11 +12,17 @@ namespace mw
 	{
 
 	public:
-		NewReminderFrame(wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxT("Task"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(450, 400), long style = wxDEFAULT_FRAME_STYLE | wxFRAME_FLOAT_ON_PARENT | wxTAB_TRAVERSAL);
+		NewReminderFrame(wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxT("Reminder"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(450, 400), long style = wxDEFAULT_FRAME_STYLE | wxFRAME_FLOAT_ON_PARENT | wxTAB_TRAVERSAL);
 		~NewReminderFrame();
-
+		
 		void SetReminder(const mw::Reminder& reminder);
+	private:
+		void OnSaveButton(wxCommandEvent& event);
 		mw::Reminder m_reminder;
+		// Member variables for the form components
+		wxTextCtrl* titleInput;
+		wxTextCtrl* textInput;
+		wxButton* save_button;
 
 	};
 }
--- masterwork/src/NewReminderFrame.cpp
+++ masterwork/src/NewReminderFrame.cpp
@@ -3,7 +3,24 @@
 mw::NewReminderFrame::NewReminderFrame(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style) 
 	: wxFrame(parent, id, title, pos, size, style)
 {
+    // Create input fields and button
+    titleInput = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_PROCESS_ENTER);
+    textInput = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE);
+    save_button = new wxButton(this, wxID_ANY, wxT("Save"));
 
+    // Create a vertical sizer to arrange the components
+    wxBoxSizer* vSizer = new wxBoxSizer(wxVERTICAL);
+    vSizer->Add(new wxStaticText(this, wxID_ANY, wxT("Title:")), 0, wxALL, 5);
+    vSizer->Add(titleInput, 0, wxEXPAND | wxALL, 5);
+    vSizer->Add(new wxStaticText(this, wxID_ANY, wxT("Text:")), 0, wxALL, 5);
+    vSizer->Add(textInput, 1, wxEXPAND | wxALL, 5);
+    vSizer->Add(save_button, 0, wxALIGN_RIGHT | wxALL, 5);
+
+    // Set the sizer for the frame
+    this->SetSizer(vSizer);
+
+    // Connect the Save button click event to the event handler
+    save_button->Bind(wxEVT_BUTTON, &mw::NewReminderFrame::OnSaveButton, this);
 
 }
 
@@ -15,3 +32,19 @@ void mw::NewReminderFrame::SetReminder(const mw::Reminder& reminder)
 {
 	m_reminder = reminder;
 }
+
+void mw::NewReminderFrame::OnSaveButton(wxCommandEvent& event)
+{
+    wxString title = titleInput->GetValue();
+    wxString text = textInput->GetValue();
+    m_reminder.title = std::string(title.mb_str());
+    m_reminder.text = std::string(text.mb_str());
+    m_reminder.status = ReminderStatus::ACTIVE;
+
+    // You can continue setting other reminder properties here
+
+
+
+    this->Close();
+}
+
